6d2c7ecb011db19cd90190256d9233f3db8c6cca
Dwight Nodolf <dnodolf@gmail.com>
2020-04-23 07:00:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 def prime?(int)
-  (2..int - 1).each {|x| return false if (int % x) == log10 || int.negative? == true}
+  (2..int - 1).each {|x| return false if (int % x) == 0 || int.negative? == true}
   true 
   
   
